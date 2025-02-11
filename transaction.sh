e67b286119269341a9e1d4924fca9ffa16cb833b
DevopsProject4u <prudhvijavajala1@gmail.com>
2025-02-11 20:57:21 +0530

    creating transaction.sh

---
+++ transaction.sh
@@ -0,0 +1,12 @@
+TASK-1: Add index.html file in Project-Management repository
+git clone https://github.com/GoudSagar/devops-automate-work.git
+cd devops-automate-work
+git branch -> main branch
+vi index.html -> content -> :wq
+git add .
+git commit -m "Ticket - description/messages"
+git remote -v
+origin https://github.com/GoudSagar/devops-automate-work.git(fetch)
+origin https://github.com/GoudSagar/devops-automate-work.git(push)
+git push origin main
+Task-2:  Update the sample.html with new changes for upcoming release
