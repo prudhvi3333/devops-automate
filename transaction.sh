0aa4b26b23b410d0601054004ed44e78212a6de4
DevopsProject4u <prudhvijavajala1@gmail.com>
2025-02-11 21:12:31 +0530

    Revert "pushing the modified file pulled from company account to personal"

--- transaction.sh
+++ transaction.sh
@@ -10,20 +10,3 @@ origin https://github.com/GoudSagar/devops-automate-work.git(fetch)
 origin https://github.com/GoudSagar/devops-automate-work.git(push)
 git push origin main
 Task-2:  Update the sample.html with new changes for upcoming release
-git remote add upstream https://github.com/devopscloudtrainer/devops-automate-work.git
-
-git remote -v
-origin https://github.com/GoudSagar/devops-automate-work.git(fetch)
-origin https://github.com/GoudSagar/devops-automate-work.git(push)
-upstream https://github.com/devopscloudtrainer/devops-automate-work.git(fetch)
-upstream https://github.com/devopscloudtrainer/devops-automate-work.git(push)
-
-git pull upstream main  
-
-vi sample.html -> add content -> :wq
-git add .
-git commit -m "ticket - description/messages"
-git push origin main
-============================================================================================================================
-Fork Mechanism: 
-
